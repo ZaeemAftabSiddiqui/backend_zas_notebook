dbc78c7feccf7cfe3d5040d9cd0ac7d99c227657
Zaeem Aftab Siddiqui <ZaeemAftabSiddiqui@gmail.com>
2022-07-13 16:06:17 +0500

    connect with auth and notes

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ connectToMongo();
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+//Available Routes
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/notes", require("./routes/notes"));
+
 app.get("/", (req, res) => {
   res.send("Hello Zaeem!");
 });
