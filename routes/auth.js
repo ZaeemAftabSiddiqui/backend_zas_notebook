55bbcc3efbf1ac9e6833a7bb0bd1f7af0e7b5af9
Zaeem Aftab Siddiqui <ZaeemAftabSiddiqui@gmail.com>
2022-07-18 02:19:51 +0500

    apply bcrypt for made hash of password and build token with secret

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
+
 const { body, validationResult } = require("express-validator");
 const User = require("../models/User");
-
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = "swswswswswsw";
 //Create a User using : POST "/api/auth/createUser". No login required
 router.post(
   "/createUser",
@@ -19,21 +22,31 @@ router.post(
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    // check wheather the user email exists already
     try {
+      // check wheather the user email exists already
       let user = await User.findOne({ email: req.body.email });
       if (user) {
         return res
           .status(400)
           .json({ error: "sorry with this email already exists" });
       }
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
       //Create a new user
       user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: secPass,
       });
-      res.json(user);
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authToken = jwt.sign(data, JWT_SECRET);
+
+      //res.json(user);
+      res.json({ authToken });
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Some Error occured");
