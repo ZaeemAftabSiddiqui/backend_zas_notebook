f57acd15a04d3c83d0823ef5d7c612f114632785
Zaeem Aftab Siddiqui <ZaeemAftabSiddiqui@gmail.com>
2023-01-12 00:25:53 +0500

    install mangoose new version and change mongoURI

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
-const mongoURI = "mongodb://localhost:27017";
+const mongoURI = "mongodb://127.0.0.1:27017/ZASNotebook";
 
 const connectToMongo = () => {
+  mongoose.set("strictQuery", true);
   mongoose.connect(mongoURI, () => {
     console.log("connected to mongo Succesfully");
   });
